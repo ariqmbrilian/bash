1d2169f0e675af92ed53c0e4a7d34fc1999b2547
ariqmbrilian <ariqmbrilian@gmail.com>
2021-11-07 21:02:52 +0700

    bash

--- printNumber.sh
+++ printNumber.sh
@@ -10,4 +10,4 @@ for i in {1..100..1}; do echo "This is number $i"; done
 for i in {1..100..2}; do echo "This is number $i"; done
 
 ## Invinite loop
-while true; do echo "Unable to stop"; done
+while true; do echo "Unable to stop"; done  
