330415c87228e1d058e199647e8ee8f129d927b3
ariqmbrilian <ariqmbrilian@gmail.com>
2021-11-07 20:20:55 +0700

    bash

--- printNumber.sh
+++ printNumber.sh
@@ -8,3 +8,6 @@ for i in {1..100..1}; do echo "This is number $i"; done
 
 ## Print number 1-100 with even pattern
 for i in {1..100..2}; do echo "This is number $i"; done
+
+## Invinite loop
+while true; do echo "Unable to stop"; done
