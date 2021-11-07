c7fb592f3694578d1b5148e137df434c984dfc44
ariqmbrilian <ariqmbrilian@gmail.com>
2021-11-07 20:19:17 +0700

    bash

---
+++ printNumber.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Print number 1-100
+for i in {1..100}; do echo "This is number $i"; done
+
+## Print number 1-100 with odd pattern
+for i in {1..100..1}; do echo "This is number $i"; done
+
+## Print number 1-100 with even pattern
+for i in {1..100..2}; do echo "This is number $i"; done
